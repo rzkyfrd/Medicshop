8f2a749134106953692b4ce4d9066d51eb4a64e7
irfansyah-r <irfansyah.rizal.20@gmail.com>
2024-03-07 14:20:00 +0700

    Fix product list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public $pagination = 8;
+
+    public function index(Request $request)
     {
-        $product = Product::all();
+        $keyword = $request->keyword ?? '';
+        $categories = json_decode($request->categories ?? '[]', true);
+
+        $product = Product::when($keyword, function ($query, $value) {
+            $query->where('name', $value);
+        })->when($categories, function ($query, $value) {
+            $query->whereIn('category_id', $value);
+        })->paginate($this->pagination);
+
         $category = Category::all();
-        return view('home', compact('product', 'category'));
+        return view('home', compact('product', 'category', 'keyword', 'categories'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,122 +1,6 @@
 <x-app-layout>
-    <div class="py-12 grid grid-cols-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-4 col-span-3">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <section>
-                        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-2 lg:px-8">
-
-                            <div class="mt-2 flex items-center justify-between">
-                                <div class="flex rounded">
-                                    <p class="text-sm text-gray-500">Showing <span> 4 </span> of 40</p>
-                                </div>
-                                <div>
-                                    <form class="flex items-center max-w-sm mx-auto">
-                                        <label for="simple-search" class="sr-only">Search</label>
-                                        <div class="relative w-full">
-                                            <input type="text" id="simple-search"
-                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                placeholder="Search product name..." required />
-                                        </div>
-                                        <button type="submit"
-                                            class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" stroke-linecap="round"
-                                                    stroke-linejoin="round" stroke-width="2"
-                                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                            </svg>
-                                            <span class="sr-only">Search</span>
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-
-                            <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-
-                                @foreach ($product as $row)
-                                    <li class="border-2 border-gray-100">
-                                        <a href="#" class="group relative block overflow-hidden">
-                                            <img src="{{ $row->image }}" alt=""
-                                                class=" h-[200px] max-h-[200px] mx-auto transition duration-500 group-hover:scale-105 sm:h-72" />
-
-                                            <div class="relative border-t-4 border-gray-100 bg-white px-3 py-2 mt-1">
-                                                <h3 class="mt-1 text-lg font-medium text-gray-900 font-bold">
-                                                    {{ $row->name }}
-                                                </h3>
-                                                <p class="mt-1.5 text-sm text-gray-700">@currency($row->price)</p>
-                                                <form class="mt-4">
-                                                    <input type="hidden" name="">
-                                                    <button
-                                                        class="block w-full rounded bg-red-600 text-white p-4 hover text-sm font-medium transition hover:scale-105">
-
-                                                        Add to Cart
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-
-                            <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
-                                <li>
-                                    <a href="#"
-                                        class="inline-flex size-8 items-center justify-center rounded border border-gray-100">
-                                        <span class="sr-only">Prev Page</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20"
-                                            fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#"
-                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
-                                        1
-                                    </a>
-                                </li>
-
-                                <li class="block size-8 rounded border-black bg-black text-center leading-8 text-white">
-                                    2</li>
-
-                                <li>
-                                    <a href="#"
-                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
-                                        3
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#"
-                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
-                                        4
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#"
-                                        class="inline-flex size-8 items-center justify-center rounded border border-gray-100">
-                                        <span class="sr-only">Next Page</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20"
-                                            fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ol>
-                        </div>
-                    </section>
-
-                </div>
-            </div>
-        </div>
-        <div class="mx-auto w-full sm:px-6 lg:px-4">
+    <div class="py-12 grid grid-cols-4 sm:px-16">
+        <div class="mx-auto w-full sm:px-6 lg:px-2">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <section>
@@ -186,7 +70,11 @@
                         <div class="text-left px-4">
                             @foreach ($category as $row)
                                 <div class="mb-2">
-                                    <a href="">{{ $row->name }}</a> <br>
+                                    {{-- <a href="">{{ $row->name }}</a> <br> --}}
+                                    <div class="flex justify-between">
+                                        <label class="cursor-pointer hover:font-semibold" for="category{{$row->id}}">{{ $row->name }}</label>
+                                        <input onchange="submitForm()" {{ in_array($row->id, $categories) ? 'checked' : '' }} class="rounded focus:ring-0 cursor-pointer" type="checkbox" value="{{ $row->id }}" name="category" id="category{{$row->id}}">
+                                    </div>
                                 </div>
                             @endforeach
                         </div>
@@ -194,5 +82,145 @@
                 </div>
             </div>
         </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-4 col-span-3">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <section>
+                        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-2 lg:px-8">
+
+                            <div class="mt-2 flex items-center justify-between">
+                                <div class="flex rounded">
+                                    {{-- <p class="text-sm text-gray-500">Showing <span> 4 </span> of 40</p> --}}
+                                </div>
+                                <div>
+                                    <div class="flex items-center max-w-sm mx-auto">
+                                        <label for="search" class="sr-only">Search</label>
+                                        <div class="relative w-full">
+                                            <input type="text" id="search" onkeydown="event.key == 'Enter' ? submitForm() : ''"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="Search product name..." required value="{{ $keyword }}" />
+                                        </div>
+                                        <button type="submit" onclick="submitForm()"
+                                            class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            </svg>
+                                            <span class="sr-only">Search</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+
+                                @foreach ($product as $row)
+                                    <li class="border-2 border-gray-100">
+                                        <a href="#" class="group relative block overflow-hidden">
+                                            <img src="{{ $row->image }}" alt=""
+                                                class=" h-[200px] max-h-[200px] mx-auto transition duration-500 group-hover:scale-105 sm:h-72" />
+
+                                            <div class="relative border-t-4 border-gray-100 bg-white px-3 py-2 mt-1">
+                                                <h3 class="mt-1 text-lg font-medium text-gray-900 font-bold">
+                                                    {{ $row->name }}
+                                                </h3>
+                                                <p class="mt-1.5 text-sm text-gray-700">@currency($row->price)</p>
+                                                <form class="mt-4">
+                                                    <input type="hidden" name="">
+                                                    <button
+                                                        class="block w-full rounded bg-red-600 text-white p-4 hover text-sm font-medium transition hover:scale-105">
+
+                                                        Add to Cart
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+
+                            <div class="w-full mt-4">
+                                {{ $product->links() }}
+                            </div>
+                            {{-- <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
+                                <li>
+                                    <a href="#"
+                                        class="inline-flex size-8 items-center justify-center rounded border border-gray-100">
+                                        <span class="sr-only">Prev Page</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20"
+                                            fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#"
+                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
+                                        1
+                                    </a>
+                                </li>
+
+                                <li class="block size-8 rounded border-black bg-black text-center leading-8 text-white">
+                                    2</li>
+
+                                <li>
+                                    <a href="#"
+                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
+                                        3
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#"
+                                        class="block size-8 rounded border border-gray-100 text-center leading-8">
+                                        4
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#"
+                                        class="inline-flex size-8 items-center justify-center rounded border border-gray-100">
+                                        <span class="sr-only">Next Page</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20"
+                                            fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ol> --}}
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+        <form action="" id="mainForm">
+            <input type="hidden" name="keyword" id="keyword" value="{{ $keyword }}">
+            <input type="hidden" name="categories" id="categories">
+        </form>
     </div>
+    <script type="module">
+        window.submitForm = function () {
+            let categories = [];
+            $("input:checkbox[name=category]:checked").each(function(){
+                categories.push($(this).val());
+            });
+            $('#keyword').val($('#search').val());
+
+            if(categories.length > 0 || $('#keyword').val() !== ''){
+                $('#categories').show();
+                $('#categories').val(JSON.stringify(categories));
+            }else{
+                $('#categories').hide();
+            }
+            $('#mainForm').submit();
+        }
+    </script>
 </x-app-layout>
